b56e9394000a09e967c2f88efb2ce64d5a22867f
wingferno <surrealfootbal.ice@gmail.com>
2026-04-22 20:00:43 -0400

    assign numbers to exams

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
 exams = pd.read_excel("exams.xlsx", sheet_name = 0, usecols = "A:C", header=None)
-print(exams.values)
+exams = exams.values[0:10]
+print(exams)
+
+exams_dictionary = {}
+exam_count = 0
+for student in exams:
+    for exam in student:
+        if exam not in exams_dictionary:
+            exams_dictionary[exam] = exam_count
+            exam_count += 1
+print(exams_dictionary, exam_count)        
